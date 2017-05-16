0a8844efc4ff4ebb0af40302f22d5014a4956c7f
Roman Akash <roman.akash@yahoo.com>
2017-05-16 21:57:38 +0530

    Final Date change

--- src/handlers/daily.js
+++ src/handlers/daily.js
@@ -1,13 +1,17 @@
 import mongojs from 'hapi-mongojs';
-const ObjectId = require('mongodb').ObjectID;
+import moment from 'moment';
 
 const getDailyQuotes = (request, reply) => {
     const daily = mongojs.db().collection('daily');
+    const year = moment().year();
+    const days = moment().daysInMonth();
     let month = request.params.month;
-    daily.find((err, dailies) => {
-        if (err) {
-            console.error(err);
-        }
+    daily.find({
+        date: { $gte: new Date(year, month, 1), $lt: new Date(year, month, days)}
+    },  (err, dailies) => {
+            if (err) {
+                console.error(err);
+            }
         reply(dailies)
     })
 };
